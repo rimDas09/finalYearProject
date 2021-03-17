bb8944ce10e09cfcb5d1569462d7420601b2da95
unknown <rim.nk00@gmail.com>
2021-03-17 08:32:05 -0700

    adding files

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService, UserService } from '@app/_services';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.less']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private router: Router,
+      private authenticationService: AuthenticationService,
+      private userService: UserService,
+      //private alertService: AlertService
+  ) {
+      if (this.authenticationService.currentUserValue) {
+          this.router.navigate(['/']);
+      }
+  }
+
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          username: ['', Validators.required],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // reset alerts on submit
+    //  this.alertService.clear();
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      this.userService.register(this.registerForm.value)
+          .pipe(first())
+          .subscribe(
+              data => {
+                  //this.alertService.success('Registration successful', true);
+                  console.log("success");
+                  
+                  this.router.navigate(['/login']);
+              },
+              error => {
+                  //this.alertService.error(error);
+                  console.log("fail");
+                  
+                  this.loading = false;
+              });
+  }
+
+}
